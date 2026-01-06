b70c524963fa05f251d790dec4d52daa44a64a87
Syam Sundar Kudumala <syamsundarkudumala@gmail.com>
2026-01-06 15:36:02 +0530

    fix: make app.listen render-safe

--- server/index.ts
+++ server/index.ts
@@ -86,8 +86,9 @@ app.use((req, res, next) => {
   // It is the only port that is not firewalled.const PORT = 5000;
 const PORT = Number(process.env.PORT) || 5000;
 
-app.listen(PORT, "127.0.0.1", () => {
-  console.log(`Server running on http://127.0.0.1:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
 
 })();
